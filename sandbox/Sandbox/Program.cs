f7c9d05c2b728ad34a043ecf9d13eb22988d7f2f
Cherry-Machado <Cherry5467@gmail.com>
2025-01-14 12:53:04 -0300

    Practicing in Sandox Program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Indique su edad: ");
+        int edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Su edad es: " + edad);
+
+        Console.Write("Indique su nombre: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine($"Su nombre es: {nombre} ");
     }
 }
