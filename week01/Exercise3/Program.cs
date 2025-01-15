3b2fd8fbef9d6b35d490642933b770b8702444e9
Cherry-Machado <Cherry5467@gmail.com>
2025-01-14 21:38:24 -0300

    Excercice 3 week1 first version

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Author: CherryMachado
+        //Purpose: Guess My Number game Program.
+        //Restrictions: None
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.WriteLine("Welcome to the Guess My Number game!");
+        int magicNumber = 0;
+        int guess = 0;
+        do 
+        { 
+            Console.Write("What is the magic number? ");
+            magicNumber = int.Parse(Console.ReadLine());
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Your guess is too high.");
+            }
+            else
+            {
+                Console.WriteLine("Your guess is too low.");
+            }
+        } while (magicNumber != guess);
     }
 }
