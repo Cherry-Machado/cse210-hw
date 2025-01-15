8e8e2b674070d43ae5f11d3604cfea423124031c
Cherry-Machado <Cherry5467@gmail.com>
2025-01-15 00:51:52 -0300

    Exercise 4 week1 final version

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,10 +7,11 @@ class Program
     {
         //Author: CherryMachado
         //Purpose: Appending numbers in a list.
-        
+
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
         Console.WriteLine("");
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        // Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.WriteLine("Enter a list of numbers, (positive and negative) type 0 when finished.");
         List<int> numbers = new List<int>();
         int number = 1;
         while (number != 0)
@@ -22,18 +23,36 @@ class Program
             }
         }
         float sum = 0;
-        int largestNumberInTheList = numbers[0];
+        int largestNumber = numbers[0];
         foreach (int num in numbers)
         {   
             sum += num;
-            if (num > largestNumberInTheList)
+            if (num > largestNumber)
             {
-                largestNumberInTheList = num;
+                largestNumber = num;
             }
         }
+        int theSmallestPositiveNumber = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > 0 && num < theSmallestPositiveNumber)
+            {
+                theSmallestPositiveNumber = num;
+            }
+        }
+
+       
         Console.WriteLine($"The sum of the numbers is: {sum}");
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
-        Console.WriteLine($"The largest number in the list is: {largestNumberInTheList}");
+        Console.WriteLine($"The largest number in the list is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number in the list is: {theSmallestPositiveNumber}");
+        Console.Write("The sorted list is: ");
+        numbers.Sort();
+        foreach (int num in numbers)
+        {
+            Console.Write(num);
+            Console.Write(" ");
+        }
     }
 }
