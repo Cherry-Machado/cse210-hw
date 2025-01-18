095d4dceb5f83731df1711bd05b7774d2b04531f
Cherry-Machado <Cherry5467@gmail.com>
2025-01-18 12:31:22 -0300

    Updating AddEntry() in Journal.cs file.

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,47 +1,37 @@
 using System.Security.Cryptography.X509Certificates;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 public  class Journal
 {
-    public List<Entry> _entries;
+    public List<Entry> _entries = new List<Entry>();
     public void AddEntry(Entry newEntry)
     {
         _entries.Add(newEntry);
     }
     public void DisplayAll()
     {
-        foreach (Entry entry in _entries)
+        if (_entries.Count == 0)
         {
-            entry.Display();
+            Console.WriteLine("The journal is empty.");
+            return;
+        }
+        foreach (var entry in _entries)
+        {
+            Console.WriteLine($"Date: {entry._date} - Prompt: {entry._promptText}");
+            Console.WriteLine($"Entry: {entry._entryText}");
+            Console.WriteLine("-------------------------------------------------------");
         }
     }
 
     public void SaveToFile(string file)
     {
-        using (StreamWriter writer = new StreamWriter(file))
-        {
-            foreach (Entry entry in _entries)
-            {
-                writer.WriteLine(entry._date);
-                writer.WriteLine(entry._promptText);
-                writer.WriteLine(entry._entryText);
-            }
-        }
+        
     }
 
     public void LoadFromFile(string file)
     {
-        using (StreamReader reader = new StreamReader(file))
-        {
-            while (!reader.EndOfStream)
-            {
-                Entry entry = new Entry();
-                entry._date = reader.ReadLine();
-                entry._promptText = reader.ReadLine();
-                entry._entryText = reader.ReadLine();
-                _entries.Add(entry);
-            }
-        }
+        
     }
 }
