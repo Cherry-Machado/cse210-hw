c96c60f1a5b1e5c06ebd752fdc8f7952c0905c74
Cherry-Machado <Cherry5467@gmail.com>
2025-01-18 12:51:34 -0300

    Finished SaveToFile() in Journal.cs

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -2,6 +2,7 @@ using System.Security.Cryptography.X509Certificates;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Formats.Asn1;
 
 public  class Journal
 {
@@ -27,7 +28,14 @@ public  class Journal
 
     public void SaveToFile(string file)
     {
-        
+        using (StreamWriter writer = new StreamWriter(file))
+        {
+            foreach (var entry in _entries)
+            {
+                writer.WriteLine($" {entry._date}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+        Console.WriteLine($"Journal saved to file {file}.");
     }
 
     public void LoadFromFile(string file)
