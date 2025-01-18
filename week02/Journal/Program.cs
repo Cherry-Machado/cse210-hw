2a26026b8fa3bd261e414af8d9bbdd6fe2dfce98
Cherry-Machado <Cherry5467@gmail.com>
2025-01-18 12:34:44 -0300

    Updating Program.cs switch options.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -63,44 +63,6 @@ class Program
                     break;
             }
         } while (menu != 5);
-        Console.WriteLine("1. Write in Journal");
-        Console.WriteLine("2. Display Journal");
-        Console.WriteLine("3. Load file to Journal");
-        Console.WriteLine("4. Save to file");
-        Console.WriteLine("5. Quit");
-        int menu;
-        do
-        {
-            Console.Write("Please select an option: ");
-            menu = Int32.Parse(Console.ReadLine());
-            switch (menu)
-            {
-                case 1:
-                    Display();
-                    break;
-                case 2:
-                    Console.WriteLine("Display");
-                    break;
-                case 3:
-                    Console.WriteLine("Load");
-                    break;
-                case 4:
-                    Console.WriteLine("Save");
-                    break;
-                case 5:
-                    Console.WriteLine("Quit");
-                    break;
-                default:
-                    Console.WriteLine($"Your option is incorrect. Please try again.");
-                    break;
-                static void Display()
-                {
-                    Console.WriteLine("Display Journal");
-                }
-            }
-        } while (menu != 5);
-        Journal theJournal = new Journal();
-        Entry anEntry = new Entry();
-        PromptGenerator thePromptGenerator = new PromptGenerator();
+        
     }
 }
