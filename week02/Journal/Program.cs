d284806e8be155ef2694bf19fa9a023f99386c40
Cherry-Machado <Cherry5467@gmail.com>
2025-01-18 12:22:27 -0300

    Finishedf Option 1

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -10,7 +10,63 @@ class Program
         Console.WriteLine("Welcome to the Journal Program!");
         // This program will help you to start a Joournal by asking you a series of questions and save your answers to a file.
         Console.WriteLine("");
-        
+
+        Journal myJournal = new Journal();
+        PromptGenerator myPromptGenerator = new PromptGenerator();
+
+        int menu;
+        do
+        {
+            Console.WriteLine("1. Write in Journal");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Load file to Journal");
+            Console.WriteLine("4. Save to file");
+            Console.WriteLine("5. Quit");
+            Console.Write("Please select an option: ");
+            menu = Int32.Parse(Console.ReadLine());
+            switch (menu)
+            {
+                case 1:
+                    string prompt;
+                    do
+                    {
+                        prompt = myPromptGenerator.GetRandomPrompt();
+                        Console.WriteLine($"Prompt: {prompt}");
+                        prompt = myPromptGenerator.GetRandomPrompt();
+                        Console.Write("Your answer: ");
+                        string userEntry = Console.ReadLine();
+
+                        Entry newEntry = new Entry()
+                        {
+                            _date = DateTime.Now.ToString("MM/dd/yyyy"),
+                            _promptText = prompt,
+                            _entryText = userEntry
+                        };
+                        myJournal.AddEntry(newEntry);
+                        Console.Write("Press '1' to add another entry or any othe to exit");
+                    } while (Console.ReadLine().ToLower() == "1");
+                    break;
+                case 2:
+                    myJournal.DisplayAll();
+                    break;
+                case 3:
+                    Console.Write("Enter the file name: ");
+                    string file = Console.ReadLine();
+                    myJournal.LoadFromFile(file);
+                    break;
+                case 4:
+                    Console.Write("Enter the file name: ");
+                    file = Console.ReadLine();
+                    myJournal.SaveToFile(file);
+                    break;
+                case 5:
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Your option is incorrect. Please try again.");
+                    break;
+            }
+        } while (menu != 5);
         Console.WriteLine("1. Write in Journal");
         Console.WriteLine("2. Display Journal");
         Console.WriteLine("3. Load file to Journal");
