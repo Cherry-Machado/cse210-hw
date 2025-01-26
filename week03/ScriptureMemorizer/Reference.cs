e1496e985bc74fc6310dbab7815926d2143065d4
Cherry-Machado <Cherry5467@gmail.com>
2025-01-26 00:23:07 -0300

    Creating Reference.cs

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Reference
+{
+    private string book;
+    private int chapter;
+    private int verse;
+    private int endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        this.book = book;
+        this.chapter = chapter;
+        this.verse = verse;
+        this.endVerse = verse;
+    }
+}
