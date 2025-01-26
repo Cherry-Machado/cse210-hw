358509588982ff4c16e419ade343b433de83c4d8
Cherry-Machado <Cherry5467@gmail.com>
2025-01-26 00:10:05 -0300

    Creating Scripture.cs

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Scripture
+{
+    private Reference reference;
+    private List<WaitForChangedResult> words;
+    public Scripture(Reference reference, string scriptureText)
+    {
+        this.reference = reference;
+        words = new List<Word>();
+
+        foreach (var word in scriptureText.Split(' '))
+        {
+            words.Add(new Word(word));
+        }
+    }
+}
