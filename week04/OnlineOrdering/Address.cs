fe44b5f5c66bfed23619cce1821f4fe66129216b
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 14:45:52 -0300

    Creating Adress Class

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+// Class representing an Address
+public class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _stateOrProvince;
+    private string _country;
+
+    
+}
