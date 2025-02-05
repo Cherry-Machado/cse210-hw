9ed0bbff286c8a19f8483fd524c7c2a67cb0b3cc
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 19:13:46 -0300

    ending the MathAssignment class

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -3,6 +3,22 @@ using System.Collections.Generic;
 
 // Creating the MathAssignment Class
 
-public class MathAssignment : Assignment{
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    // Building the Constructor with 4 parameters, 2 of them heritaged from
+    // Assignment class using "base".
     
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
 }
