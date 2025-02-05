9487a53ca9582dc462c38d42fe475b72de2ba7d9
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 18:51:53 -0300

    testing the Assignment class from Program Class

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine("");
+
+        Assignment assigment = new Assignment("Jose", "Matematicas");
+        string name = assigment.GetStudentName();
+        string topico = assigment.GetTopic();
+        Console.WriteLine(name);
+        Console.WriteLine(topico); 
     }
 }
