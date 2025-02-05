9a59eefd6d9538a5e91f357ea9e06bf9022503d4
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 15:49:38 -0300

    Updating

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -10,5 +10,29 @@ class Program
         // Create adresses
         Address address1 = new Address("123 Main St", "New York", "NY", "USA");
         Address address2 = new Address("456 Elm St", "Toronto", "ON", "Canada");
+
+        //Create customers
+        Customer customer1 = new Customer("John Doe", address1);
+        Customer customer2 = new Customer("Jane Smith", address2);
+
+        //Create products
+        Product product1 = new Product("Laptop", "LP123", 1000, 1);
+        Product product2 = new Product("Mouse", "MS456", 25, 2);
+        Product product3 = new Product("Keyboard", "KB789", 50, 1);
+        Product product4 = new Product("Monitor", "MN101", 200, 1);
+        Product product5 = new Product("Headphones", "HP202", 50, 3);
+
+        // Create orders
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+        order1.AddProduct(product3);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product4);
+        order2.AddProduct(product5);
+
+        
+
     }
 }
