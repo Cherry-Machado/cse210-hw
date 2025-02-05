383fe0ed72523f21e8b3a16e61822a747f7a9b26
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 19:48:18 -0300

    Creating the WritingAssignment Class.

---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+//Creating the WrittingAssignment Class
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    // Creating the Constructor with 3 parameters, 2 of them
+    // coming from the base Class Assignment.
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    // Creating the GetWritingInformation() Method
+}
