c6627722e665fa7870c6220cd784c5ffd5ed8630
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 15:26:45 -0300

    Updating, adding Methods

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -6,4 +6,20 @@ public class Order
 {
     private List<Product> _products;
     private Customer _customer;
+
+    // Constructor
+    public Order(Customer customer)
+    {
+        _products = new List<Product>();
+        _customer = customer;
+    }
+
+    // Method to add a product to the order
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    //Method to calculate the total price of the order
+    
 }
