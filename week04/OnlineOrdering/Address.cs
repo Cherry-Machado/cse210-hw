ce49fce883ac7d1251696566ce5bd7f29080105a
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 14:52:31 -0300

    updating constructor

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -9,5 +9,15 @@ public class Address
     private string _stateOrProvince;
     private string _country;
 
+    // Constructor
+    public Address(string streetAddress, string city, string stateOrProvince, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateOrProvince = stateOrProvince;
+        _country = country;
+
+    }
+
     
 }
