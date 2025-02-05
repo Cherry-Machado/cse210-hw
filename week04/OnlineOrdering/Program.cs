908fe0b1e5a3d74c8f9065406faf5d10e363c4ba
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 15:43:27 -0300

    upadating

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Console.WriteLine("");
+
+        // Create adresses
+        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
+        Address address2 = new Address("456 Elm St", "Toronto", "ON", "Canada");
     }
 }
