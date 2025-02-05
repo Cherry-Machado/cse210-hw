9759e8556f6afd3fd3e25d56aea326e5a505aa91
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 15:17:12 -0300

    ending Product class

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -7,5 +7,25 @@ public class Product
     private string _productId;
     private double _prince;
     private int _quantity;
-    
+
+    // Constructor
+    public Product(string name, string productId, double price, int quatity)
+    {
+        _name = name;
+        _productId = productId;
+        _prince = price;
+        _quantity = quatity;
+    }
+
+    // Method to calculate the total cost of the product
+    public double CalculateTotalCost()
+    {
+        return _prince * _quantity;
+    }
+
+    // Method to get the product's name and ID fot the packing label
+    public string GetPackingLabelInfo()
+    {
+        return $"Product: {_name} (ID: {_productId})";
+    }
 }
