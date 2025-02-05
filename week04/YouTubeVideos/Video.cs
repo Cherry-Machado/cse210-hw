65f924aa27f3611fe8473eb2777e2422e9a49959
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 00:58:47 -0300

    Creating Video Class

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -13,6 +13,21 @@ public class Video
     {
         _title = title;
         _author = author;
-        _lengthInSeconds = lengthInSeconds
+        _lengthInSeconds = lengthInSeconds;
+        _comments = new List<Comment>();
     }
+
+    // Method to add a comment to the video
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    //Method to get the number of comments
+    public int GetNumbersOfComments()
+    {
+        return _comments.Count;
+    }
+
+    // Metho
 }
