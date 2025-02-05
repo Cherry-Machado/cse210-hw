35ef1a1688b4d8db6dcdbc7d7b38aa1a90c5afcf
Cherry-Machado <Cherry5467@gmail.com>
2025-02-05 15:03:04 -0300

    creating constructor

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+// Class representing a Customer
+public class Customer
+{
+    private string _name;
+    private Address _address;
+
+    // Constructor
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+    
+}
