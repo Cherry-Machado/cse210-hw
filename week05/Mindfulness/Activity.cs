9c9a7eedb68bcc6083af3f06f146f30e48ee96c3
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 02:08:00 -0300

    updating Activity class

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -8,3 +8,14 @@ abstract class Activity
     protected string _description;
     protected int _duration;
 }
+
+public void DisplayStartingMessage()
+{
+    Console.WriteLine($"Welcome to the {_name}.");
+    Console.WriteLine(_description");
+    Console.WriteLine("Enter the duration of the activity in seconds: ");
+    _duration = int.Parse(Console.ReadLine());
+    Console.WriteLine("Prepare to begin...");
+    ShowSpinner(3);
+    
+}
