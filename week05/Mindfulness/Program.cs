a6976f75590078f8f450804c01d6d6bf5af8647c
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 01:40:38 -0300

    Beginning the Program Class.

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
+// CSE210 Programming with Classes.
+// Cherry Machado.
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine("");
+
+        
     }
 }
