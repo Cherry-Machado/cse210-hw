504d4495288a128b81c0c15c80f6218eb6a58c75
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 03:21:42 -0300

    Creating ListingActivity() Constructor.

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -4,6 +4,7 @@ using System.Threading;
 
 class ListingActivity : Activity
 {
+    // Creating Attributes
     private List<string> _prompts = new List<string>
     {
         "Who are people that you appreciate?",
@@ -14,5 +15,12 @@ class ListingActivity : Activity
     };
 
     private int _count;
-    
+
+    //Creating Constructor.
+    public ListingActivity()
+    {
+        _name = "Listing Activity";
+        _description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+    }
+
 }
