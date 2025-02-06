fa1aae0015815467fbd0929a7eb04632c4545266
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 02:55:15 -0300

    Creatinf ReflectingActivity() Constructor.

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -24,4 +24,10 @@ class ReflectingActvity : Activity
         "What did you learn about yourself through this experience?",
         "How can you keep this experience in mind in the future?"
     };
+
+    public ReflectingActvity()
+    {
+        _name = "Reflecting Activity";
+        _description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
+    }
 }
