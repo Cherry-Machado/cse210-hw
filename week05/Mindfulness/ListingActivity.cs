0c95cac8a219c1d73787306c2357574aaf7703ad
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 03:17:29 -0300

    Creating ListingActivity Class. Creating Atributes.

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class ListingActivity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    private int _count;
+    
+}
