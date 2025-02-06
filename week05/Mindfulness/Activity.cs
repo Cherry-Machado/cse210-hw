9831fd760babb5b61a7c965cadca409f5df08179
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 02:20:30 -0300

    Creating ShowCountDown Method.

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -37,3 +37,14 @@ public void ShowSpinner(int seconds)
     Console.WriteLine();
 }
 
+public void ShowCountDown(int seconds)
+{
+    for (int i = seconds; i> 0; i--)
+    {
+        Console.Write($"{i} ");
+        Thread.Sleep(1000);
+    }
+
+    Console.WriteLine();
+}
+
