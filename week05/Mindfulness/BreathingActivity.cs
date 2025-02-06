57e174808cc0787325979c2fcbd5f9509822cc81
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 02:30:35 -0300

    Creating BreathingActivity Constructor.

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,3 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+
+class BreathingActivity : Activity
+{
+    public BreathingActivity()
+    {
+        _name = "Breathing Activity";
+        _description = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
+    }
+    
+}
