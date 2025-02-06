688e066e8bf69cf358fd37e3600fad1c8e65ed7a
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 02:46:13 -0300

    Creating ReflectingActivity Class.

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class ReflectingActvity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
+}
