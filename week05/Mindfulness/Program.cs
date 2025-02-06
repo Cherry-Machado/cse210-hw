0208ffc67149dd622924b88a319560645e816c8f
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 01:46:15 -0300

    Creating Main menu

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -11,6 +11,22 @@ class Program
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
         Console.WriteLine("");
 
-        
+        while (true)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("2. Start reflecting activity");
+            Console.WriteLine("3. Start listing activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string choice = Console.ReadLine();
+            if (choice == "4")
+            {
+                break;
+            }
+            
+        }
+
+
     }
 }
