274fd87ba866399fd2baa216bc373c3c74fb6d96
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 02:00:57 -0300

    Creating Activity Class.

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+abstract class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+}
