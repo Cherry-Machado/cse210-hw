6e4bbbedc587d1750781df85110cb2a8e58b6673
Cherry-Machado <Cherry5467@gmail.com>
2025-02-06 02:25:44 -0300

    Ending Activity Class

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -7,44 +7,48 @@ abstract class Activity
     protected string _name;
     protected string _description;
     protected int _duration;
-}
 
-public void DisplayStartingMessage()
-{
-    Console.WriteLine($"Welcome to the {_name}.");
-    Console.WriteLine(_description");
-    Console.WriteLine("Enter the duration of the activity in seconds: ");
-    _duration = int.Parse(Console.ReadLine());
-    Console.WriteLine("Prepare to begin...");
-    ShowSpinner(3);
-}
 
-public void DisplayEndingMessage()
-{
-    Console.WriteLine("Good job!");
-    Console.WriteLine($"You have completed the {_name} for {_duration} seconds.");
-    ShowSpinner(3);
-}
-
-public void ShowSpinner(int seconds)
-{
-    for (int i = 0; i < seconds; i++)
+    public void DisplayStartingMessage()
     {
-        Console.Write(".");
-        Thread.Sleep(1000);
+        Console.WriteLine($"Welcome to the {_name}.");
+        Console.WriteLine(_description");
+    
+        Console.WriteLine("Enter the duration of the activity in seconds: ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
     }
 
-    Console.WriteLine();
-}
-
-public void ShowCountDown(int seconds)
-{
-    for (int i = seconds; i> 0; i--)
+    public void DisplayEndingMessage()
     {
-        Console.Write($"{i} ");
-        Thread.Sleep(1000);
+        Console.WriteLine("Good job!");
+        Console.WriteLine($"You have completed the {_name} for {_duration} seconds.");
+        ShowSpinner(3);
     }
 
-    Console.WriteLine();
+    public void ShowSpinner(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+
+        Console.WriteLine();
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write($"{i} ");
+            Thread.Sleep(1000);
+        }
+
+        Console.WriteLine();
+    }
+
+    public abstract void Run();
 }
 
