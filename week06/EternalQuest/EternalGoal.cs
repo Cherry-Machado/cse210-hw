dba87ca0b0b28ba389d0a0288716df40e13ae3fc
Cherry-Machado <Cherry5467@gmail.com>
2025-02-13 16:00:33 -0300

    Finishing EternalGoal class

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -3,5 +3,13 @@ namespace EternalQuest
     public class EternalGoal : Goal
     {
         public EternalGoal(string name, string description, int points) : base(name, description, points) { }
+
+        public override int RecordEvent() => _points;
+        public override bool IsComplete() => false;
+         public override string GetStringRepresentation() => 
+            $"EternalGoal|{_shortName}|{_description}|{_points}";
+
+        public override string GetDetailsString() => 
+            $"[ ] {_shortName} ({_description})";
     }
 }
