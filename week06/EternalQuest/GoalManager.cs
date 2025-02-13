c369a3dd0a00ae1fab5d7116817f57d390df43d0
Cherry-Machado <Cherry5467@gmail.com>
2025-02-13 12:58:27 -0300

    Create Menu

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -8,4 +8,29 @@ public class GoalManager
     private int _score;
     private int _level;
     private readonly Random _random = new Random();
+
+    private readonly string[] _encouragingMessages = 
+       {
+           "🎉 You're on fire!", "🚀 To infinity and beyond!", "💪 Strength and honor!",
+           "🏆 Champion status!", "🌟 Shining bright!", "🔥 Hot streak!"
+       };
+    
+    public void start()
+    {
+        while (true)
+        {
+            Console.Clear();
+            DisplayPlayerInfo();
+            Console.WriteLine("\nMenu Options:");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+            Console.WriteLine("Select an option: ");
+
+
+        }
+    }
 }
