ceb6b80fb0e258591ccee8d722c94fe16b42672f
Cherry-Machado <Cherry5467@gmail.com>
2025-02-13 16:08:29 -0300

    Finishing ChecklistGoal class

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -5,5 +5,29 @@ namespace EternalQuest
         private int _amountCompleted;
         private readonly int _target;
         private readonly int _bonus;
+
+        public ChecklistGoal(string name, string description, int points, 
+                            int target, int bonus) : base(name, description, points)
+        {
+            _target = target;
+            _bonus = bonus;
+            _amountCompleted = 0;
+        }
+
+        public override int RecordEvent()
+        {
+            _amountCompleted++;
+            int points = _points;
+             if (_amountCompleted == _target) points += _bonus;
+            return points;
+        }
+
+        public override bool IsComplete() => _amountCompleted >= _target;
+
+        public override string GetStringRepresentation() => 
+            $"ChecklistGoal|{_shortName}|{_description}|{_points}|{_bonus}|{_target}|{_amountCompleted}";
+
+        public override string GetDetailsString() => 
+            $"[{(_amountCompleted >= _target ? "X" : " ")}] {_shortName} ({_description}) -- Completed {_amountCompleted}/{_target} times";
     }
 }
