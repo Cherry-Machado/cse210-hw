0c970120327698790ced19fde5a83ae4ea7841e0
Cherry-Machado <Cherry5467@gmail.com>
2025-02-13 16:04:26 -0300

    Creating ChecklistGoal class file.

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,9 @@
+namespace EternalQuest
+{
+    public class ChecklistGoal : Goal
+    {
+        private int _amountCompleted;
+        private readonly int _target;
+        private readonly int _bonus;
+    }
+}
