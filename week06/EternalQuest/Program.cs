781a873a2ba31702242d1e2cd8e570186116d7a5
Cherry-Machado <Cherry5467@gmail.com>
2025-02-13 12:40:47 -0300

    Creating Program.cs file

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,13 @@
 using System;
 
+// CSE210 Programming with Classes.
+// Cherry Machado.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        Console.WriteLine("Hello World! This is my EternalQuest Project.");
+        Console.WriteLine("");
+        new GoalManager().Start();
     }
 }
