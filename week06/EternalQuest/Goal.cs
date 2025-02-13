b5777a71dc2e50e5baec2750f670a86a02aab57b
Cherry-Machado <Cherry5467@gmail.com>
2025-02-13 15:40:55 -0300

    Updating Goal.cs

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace EternalQuest
 {
     public abstract class Goal
@@ -5,5 +7,18 @@ namespace EternalQuest
         protected string _shortName;
         protected string _description;
         protected int _points;
+    
+
+        protected Goal(string name, string _description, in points)
+        {
+            _shortName = name;
+            _description = description;
+            _points = points;    
+        }
+
+        public abstract int RecordEvent();
+        public abstract bool IsComplete();
+        public virtual string GetDetailsString() => $"{_shortName}: {_description}";
+        public abstract string GetStringRepresentation();
     }
 }
