973283847b867f93473841162242deaf1ad1107d
Cherry-Machado <Cherry5467@gmail.com>
2025-02-13 21:31:16 -0300

    Finishing the Shapes.cs Class

--- week06/Shapes/Shape.cs
+++ week06/Shapes/Shape.cs
@@ -2,5 +2,23 @@ using System;
 
 //Here I use an Abstract Method
 public abstract class Shape{
-    
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    // Empty virtual function
+    public abstract double GetArea();
 }
