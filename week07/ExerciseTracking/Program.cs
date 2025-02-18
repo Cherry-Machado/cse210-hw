5acaf107545041d02186aac828a17f5567e5a434
Cherry-Machado <Cherry5467@gmail.com>
2025-02-18 19:25:38 -0300

    Created a <List> activity in Main

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -8,5 +8,13 @@ class Program
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
         Console.WriteLine("");
 
+        //Create a list of activities
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 11, 3), 30, 3.0),
+            new Cycling(new DateTime(2024, 11, 4), 45, 12.0),
+            new Swimming(new DateTime(2024, 11, 5), 60, 40)
+        };
+
     }
 }
