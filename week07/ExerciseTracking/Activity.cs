006864bd8dbeefd38411cb28b817dd0e5a00180a
Cherry-Machado <Cherry5467@gmail.com>
2025-02-18 18:31:05 -0300

    creating properties

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -7,4 +7,15 @@ public abstract class Activity
     //Atributes shared between the other classes
     private DateTime date;
     private int durationInMinutes;
+
+    //Constructor
+    public Activity(DateTime date, int durationInMinutes)
+    {
+        this.date;
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    //Properties
+    public DateTime Date => date;
+    public int DurationInMinutes => dura
 }
