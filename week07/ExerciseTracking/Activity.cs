3d5411059f70f5844c1384686e152e8fd6272f73
Cherry-Machado <Cherry5467@gmail.com>
2025-02-18 18:25:40 -0300

    Creating shared attributes

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+//This is the base Activity class
+public abstract class Activity
+{
+    //Atributes shared between the other classes
+    private DateTime date;
+    private int durationInMinutes;
+}
