2dedaf422e09ab554d2b7afbe283186cd3b07a07
Cherry-Machado <Cherry5467@gmail.com>
2025-02-18 18:44:05 -0300

    finished the Activity class

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 //This is the base Activity class
 public abstract class Activity
@@ -17,5 +18,19 @@ public abstract class Activity
 
     //Properties
     public DateTime Date => date;
-    public int DurationInMinutes => dura
+    public int DurationInMinutes => durationInMinutes;
+
+    //Abstract methods to be implemented by derived classes.
+    public abstract double GetDistance(); //This will be the Distance in miles or kilometers
+    public abstract double GetSpeed(); //This will be the Speed in mph or kph
+    public abstract double GetPace(); //This will be the Pace in minutes per mile or kilometers
+
+    // This is gonna be the Summary method (common for all activities)
+    public virtual string GetSummary()
+    {
+       return $"{date:dd MMM yyyy} {GetType().Name} ({DurationInMinutes} min) - " +
+              $"Distance {GetDistance():F1} miles, Speed {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
+        
+
+    }
 }
